2e9a4712b6a9f272fbff8e218085ef73e4e0512c
Christian Granville (RIT Student) <cxg8590@ad.rit.edu>
2016-01-30 17:47:21 -0500

    Llama level again with fix

---
+++ MorningRitual/Assets/Scripts/UI/GenerateLevels.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GenerateLevels : MonoBehaviour {
+    public GameObject SelectButtonPrefab;
+
+	// Use this for initialization
+	void Start () {
+        
+        if(PlayerPrefs.GetInt("Highest Level") < 1 || PlayerPrefs.GetInt("Highest Level") > UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings)
+        {
+            PlayerPrefs.SetInt("Highest Level", 1);
+        }
+
+        for (int i = 1; i < UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings; i++)
+        {
+            GameObject sceneButton = Instantiate(SelectButtonPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
+            sceneButton.transform.SetParent(gameObject.transform, false);
+
+            if (i <= PlayerPrefs.GetInt("Highest Level"))
+            {
+                sceneButton.GetComponentInChildren<Text>().text = "Level " + i;
+                sceneButton.GetComponent<PlayScene>().numberSetAdd = i;
+            }
+            else
+            {
+                sceneButton.GetComponentInChildren<Text>().text = "LOCKED";
+                sceneButton.GetComponent<Button>().interactable = false;
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
