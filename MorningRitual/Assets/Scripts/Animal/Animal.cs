9663aecbed0a6b839c92484974a8411b2a599c5a
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-01-29 20:51:47 -0500

    Added the sprites for the tile. Started working on the animal script.

---
+++ MorningRitual/Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Animal : MonoBehaviour {
+
+    [Header("Setup")]
+    public Transform seatTransform;
+
+    private bool isSeated = false;
+    private Rigidbody2D playerBody;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    protected virtual void OnSeated()
+    {
+
+    }
+
+    protected virtual void OnUnSeated()
+    {
+
+    }
+
+    private void Seated()
+    {
+        isSeated = true;
+        OnSeated();
+    }
+
+    private void Unseated()
+    {
+        isSeated = false;
+        OnUnSeated();
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.isTrigger || isSeated) return;
+        Transform root = other.transform.root.transform;
+        if (root.CompareTag("Player"))
+        {
+            playerBody = other.GetComponent<Rigidbody2D>();
+            Seated();
+        }
+    }
+}
