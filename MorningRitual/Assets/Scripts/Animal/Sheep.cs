92ed77b6fb17640c78daa40770933526c16c498c
Jeremy Astolfi (RIT Student) <jja6071@ad.rit.edu>
2016-01-30 14:19:55 -0500

    Added Sanchez the Sheep

--- MorningRitual/Assets/Scripts/Animal/Sheep.cs
+++ MorningRitual/Assets/Scripts/Animal/Sheep.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class Sheep : MonoBehaviour {
 
     public float bounceForce;
+    public Animator animator;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -23,6 +23,12 @@ public class Sheep : MonoBehaviour {
         {
             col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, bounceForce));
+
+            if (animator != null)
+            {
+                animator.SetTrigger("Bounce");
+            }
+            //GetComponent<Animation>().Play("SheepBounce");
         }
     }
 }
