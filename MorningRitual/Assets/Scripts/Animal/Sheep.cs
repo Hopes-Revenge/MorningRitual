34b4b5d33da8fcd652fd5211e812bca68c3b3df4
Jeremy Astolfi (RIT Student) <jja6071@ad.rit.edu>
2016-01-30 13:07:31 -0500

    Added sheep script

---
+++ MorningRitual/Assets/Scripts/Animal/Sheep.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sheep : MonoBehaviour {
+
+    public float bounceForce;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.0f, bounceForce));
+        }
+    }
+}
