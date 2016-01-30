095b922d6884510e1898e9599515b8fd0eddb594
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-01-30 02:04:25 -0500

    Worked on a tile sprite. Added new art assets and made them prefabs.

---
+++ MorningRitual/Assets/Scripts/TileSpriteHelper.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class TileSpriteHelper : MonoBehaviour {
+
+    [Header("Configure")]
+    public float tileScale = 1.0f;
+
+    private SpriteRenderer render;
+    private MaterialPropertyBlock block = new MaterialPropertyBlock();
+
+	// Use this for initialization
+	void Awake () {
+        render = GetComponent<SpriteRenderer>();
+        ApplyChanges();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ApplyChanges();
+    }
+
+    private void ApplyChanges()
+    {
+        if (render == null) return;
+        if(block == null) block = new MaterialPropertyBlock();
+        render.GetPropertyBlock(block);
+        block.SetFloat("TileScale", 1.0f / tileScale);
+        block.SetVector("ObjectSize", transform.localScale);
+        render.SetPropertyBlock(block);
+    }
+}
