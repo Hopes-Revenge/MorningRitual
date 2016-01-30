d2618745498d501cb964e74f7a7608d01bcbd850
Madison Smith (RIT Student) <ms8565@ad.rit.edu>
2016-01-30 00:33:34 -0500

    Added a level reset and small changes for speed balancing

--- MorningRitual/Assets/Scripts/PlayerController1.cs
+++ MorningRitual/Assets/Scripts/PlayerController1.cs
@@ -1,76 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController1 : MonoBehaviour {
-	private bool facingRight = true;
-	public float speed = 5.0f;
-	public float maxSpeed = 5f;
-	public float jumpSpeed = 300f;
-	private KeyCode lastHitKey;
-	bool onGround = false;
-	float thrownForce = 0;
+public class PlayerController1 : MonoBehaviour
+{
+    private bool facingRight = true;
+    public float speed = 3.0f;
+    public float maxSpeed = 3.0f;
+    public float jumpSpeed = 100f;
+    private KeyCode lastHitKey;
+    bool onGround = false;
+    float thrownForce = 0;
 
     public bool isJumping = false;
 
-	public float groundRadius = .1f;
-	public LayerMask groundObject;
-	private Vector2 lastVelocity;
+    public float groundRadius = .1f;
+    public LayerMask groundObject;
+    private Vector2 lastVelocity;
 
-	public Vector3 resetPosition;
-	public bool resetLevel = false;
-	public bool dead = false;
-
-
-	private bool isInteracting = false;
-	private bool interactionKey = false;
-	private bool nextToObject = false;
-	private GameObject interactionObject;
-	private float objectOffset = 0;
+    public Vector3 resetPosition;
+    public bool resetLevel = false;
+    public bool dead = false;
 
 
-	private float playerMovement = 0;
+    private bool isInteracting = false;
+    private bool interactionKey = false;
+    private bool nextToObject = false;
+    private GameObject interactionObject;
+    private float objectOffset = 0;
 
-	private float gatheredSpeed = 0f;
 
-	public Rigidbody2D rdBody;
+    private float playerMovement = 0;
 
-	// Use this for initialization
-	void Start () {
-		rdBody = GetComponent<Rigidbody2D> ();
-		resetPosition = rdBody.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(onGround)
+    private float gatheredSpeed = 0f;
+
+    public Rigidbody2D rdBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rdBody = GetComponent<Rigidbody2D>();
+        resetPosition = rdBody.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onGround)
         {
             isJumping = false;
         }
-		
-		if (Input.GetKeyDown (KeyCode.E)) {
-			interactionKey = !interactionKey;
-			if (interactionKey && nextToObject) {
-				isInteracting = true;
-			} else {
-				isInteracting = false;
-			}
-		}
-		if (resetLevel) {
-			Reset();
-		}
 
-	}
-	void FixedUpdate(){
-		lastVelocity = rdBody.velocity;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            interactionKey = !interactionKey;
+            if (interactionKey && nextToObject)
+            {
+                isInteracting = true;
+            }
+            else {
+                isInteracting = false;
+            }
+        }
+        if (rdBody.transform.position.y < -10)
+        {
+            Reset();
+        }
+
+    }
+    void FixedUpdate()
+    {
+        lastVelocity = rdBody.velocity;
         Vector2 tempV = new Vector2(rdBody.transform.position.x, rdBody.transform.position.y - 0.55f);
-		onGround = Physics2D.OverlapCircle (tempV, groundRadius, groundObject);
+        onGround = Physics2D.OverlapCircle(tempV, groundRadius, groundObject);
 
-		float move = Input.GetAxis ("Horizontal");
-		playerMovement = move;
-		float speed = maxSpeed;
-		if (isInteracting) {
-			speed = 2.5f;
-		}
+        float move = Input.GetAxisRaw("Horizontal");
+        playerMovement = move;
+        float speed = maxSpeed;
+        if (isInteracting)
+        {
+            speed = 2.5f;
+        }
+
+        Debug.Log(move);
 
         if (GetIsJumping() && onGround && rdBody.velocity.y >= 0)
         {
@@ -80,38 +91,44 @@ public class PlayerController1 : MonoBehaviour {
             isJumping = true;
         }
 
-        rdBody.velocity = new Vector2 ((move * speed), rdBody.velocity.y);
+        rdBody.velocity = new Vector2((move * speed), rdBody.velocity.y);
 
-		if (move > 0 && !facingRight) {
-			Flip ();
-		} else if (move < 0 && facingRight) {
-			Flip ();
-		}
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
 
-		if (isInteracting) {
-			interactionObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (rdBody.velocity.x, interactionObject.GetComponent<Rigidbody2D> ().velocity.y);
-			if(Vector3.Distance(interactionObject.transform.position, rdBody.position) > 1.6){
-				isInteracting = false;
-			}
-		}
-	}
+        if (isInteracting)
+        {
+            interactionObject.GetComponent<Rigidbody2D>().velocity = new Vector2(rdBody.velocity.x, interactionObject.GetComponent<Rigidbody2D>().velocity.y);
+            if (Vector3.Distance(interactionObject.transform.position, rdBody.position) > 1.6)
+            {
+                isInteracting = false;
+            }
+        }
+    }
 
-	void Flip(){
-		facingRight = !facingRight;
-		Vector3 scale = transform.localScale;
-		scale.x *= -1;
-		transform.localScale = scale;
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
 
-	}
+    }
 
 
-	void Reset(){
-
-	}
+    void Reset()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+    }
 
     public bool GetIsJumping()
     {
         return Input.GetAxis("Vertical") > 0.1f;
     }
 }
-
