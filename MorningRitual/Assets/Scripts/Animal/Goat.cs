4e5d28c8729e61bef18683da0c853aa7e243c921
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-01-31 01:25:37 -0500

    Mad various fixes, like the camera one.

--- MorningRitual/Assets/Scripts/Animal/Goat.cs
+++ MorningRitual/Assets/Scripts/Animal/Goat.cs
@@ -8,10 +8,9 @@ public class Goat : Animal {
     protected override void Activated()
     {
         float sign = Mathf.Sign(transform.localScale.x);
-        Debug.Log(sign);
         Vector3 startPos = transform.position + new Vector3(1.1f, 0, 0) * sign * Mathf.Abs(transform.localScale.x);
         Vector3 direction = new Vector3(sign * range, .1f, 0);
-        Debug.DrawRay(startPos, direction, Color.green, 1.0f);
+       // Debug.DrawRay(startPos, direction, Color.green, 1.0f);
         RaycastHit2D hit = Physics2D.Raycast(startPos, direction, range);
         if(hit)
         {
