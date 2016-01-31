047015f75df0528d00938c4bc88bf5d7a55869b4
Bennoni Thomas (RIT Student) <bpt3989@ad.rit.edu>
2016-01-30 21:56:35 -0500

    Chekov's Metadata

---
+++ MorningRitual/Assets/Scripts/CollectableScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableScript : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Here.");
+
+        //change the bool here
+        Destroy(this.gameObject);
+
+    }
+}
