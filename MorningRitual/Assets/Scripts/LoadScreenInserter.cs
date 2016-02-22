205a630c85d61c19ba2448f16112803459b8c362
Alec Linder (RIT Student) <afl4947@ad.rit.edu>
2016-02-21 19:54:24 -0500

    Fixed some bugs, and making the levels more consistent.

---
+++ MorningRitual/Assets/Scripts/LoadScreenInserter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScreenInserter : MonoBehaviour {
+
+    public LoadHandler loadScreen;
+
+	// Use this for initialization
+	void Start () {
+        LoadHandler loadScreenInScene = GameObject.FindObjectOfType<LoadHandler>();
+        if(loadScreenInScene == null && loadScreen != null)
+        {
+            Instantiate(loadScreen);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
